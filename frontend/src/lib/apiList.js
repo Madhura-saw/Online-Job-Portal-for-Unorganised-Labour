77e042858ebeec0b403ca5d795d9490e15a1cff0
MadhuraSawant <84861665+Madhura-saw@users.noreply.github.com>
2022-12-16 13:44:02 +0530

    Update apiList.js

--- frontend/src/lib/apiList.js
+++ frontend/src/lib/apiList.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:4444";
+export const server = "https://online-job-portal-for-unorganised-labour-ouu2.onrender.com";
 
 const apiList = {
   login: `${server}/auth/login`,
